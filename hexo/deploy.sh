c4288d0885d3df5c57707596520c72c0ad40c3d4
Gui Lin <guileen@gmail.com>
2019-12-24 19:18:22 +0800

    update deploy.sh

--- hexo/deploy.sh
+++ hexo/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git push origin hexo && hexo generate && hexo deploy
 
-ssh ubuntu@leen.ipub.io -t 'cd /var/www/; chmod 755 -R ./leengui.com'
+# ssh ubuntu@leen.ipub.io -t 'cd /var/www/; chmod 755 -R ./leengui.com'
